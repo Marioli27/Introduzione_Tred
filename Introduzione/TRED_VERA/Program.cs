b3c2c372a5cb3331765cf0578e19faf6ed74fdfc
ITT Rosselli Rasetti <rosselli.rasetti@outlook.com>
2019-10-08 09:42:33 +0200

    2 metodi uno per visualizzare i primi 100 numeri pari il secondo per i primi 100 dispari

--- Introduzione/TRED_VERA/Program.cs
+++ Introduzione/TRED_VERA/Program.cs
@@ -10,6 +10,39 @@ namespace TRED_VERA
     {
         static void Main(string[] args)
         {
+           //VisualizzaDisp();
+           //Console.WriteLine();
+           //VisualizzaPari();
+           //Console.WriteLine();
+
+            Task.Factory.StartNew(VisualizzaDisp);
+            Task.Factory.StartNew(VisualizzaPari);
+
+            Console.ReadLine();
         }
+
+        public static void VisualizzaDisp()
+        {
+            for (int i = 0; i < 200; i++)
+            {
+                if (i % 2 == 1)
+                {
+                    Console.Write(i + " ");
+                }
+            }
+            Console.ReadLine();
+        }
+
+       public static void VisualizzaPari()
+       {
+            for (int i = 0; i < 200; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    Console.Write(i + " ");
+                }
+            }
+            Console.ReadLine();
+       }
     }
 }
